c19e7643edcbaf91d854eedbb403b2b4905cec9f
Daniella Dutra <danivetdutra@gmail.com>
2022-03-10 19:01:22 -0300

    Adicionado css ao contador

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Counter from './Counter';
 
 function App() {
-  return <Counter inicialCounter={0} color="blue" />;
+  return <Counter inicialCounter={0} color="black" />;
 }
 
 export default App;
